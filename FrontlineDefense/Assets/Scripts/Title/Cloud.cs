5165827983403e1e19f64b832079d26804bf815c
kannkeii <kannkeii@outlook.jp>
2023-08-30 01:10:11 +0900

    [add]雲のアニメション作成。 [update]SceneManagerにTitleSceneとMainSceneを追加。

---
+++ FrontlineDefense/Assets/Scripts/Title/Cloud.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cloud : MonoBehaviour
+{
+    public enum CLOUD_TYPE
+    {
+        CLOUD_TYPE_LEFT = -1,
+        CLOUD_TYPE_RIGHT = 1
+    }
+
+    
+    public float cloudSpeed = 0;
+    public CLOUD_TYPE type = CLOUD_TYPE.CLOUD_TYPE_LEFT;
+
+    private float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TitleAnyKeyInput.Instance.OnAnyKeyDown += ()=> { if(speed != cloudSpeed) speed = cloudSpeed; };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToOutsideScreen();
+    }
+
+    public void MoveToOutsideScreen()
+    {
+        transform.position += (int)type * new Vector3(speed * Time.deltaTime, 0, 0);
+
+        Vector3 viewPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (viewPos.x < 0 || viewPos.x > Camera.main.pixelWidth || viewPos.y < 0 || viewPos.y > Camera.main.pixelHeight)
+        {
+            Debug.Log(gameObject.name + "=>OutSide");
+            speed = 0;
+            gameObject.SetActive(false);
+            SceneManager.LoadScene("MainScene");//LoadSceneAsync
+        }
+    }
+}
---
+++ FrontlineDefense/Assets/Scripts/Title/TitleAnyKeyInput.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnyKeyInput : MonoBehaviour
+{
+    public delegate void AnyKeyDownHandler();
+
+    public event AnyKeyDownHandler OnAnyKeyDown;
+
+    public static TitleAnyKeyInput Instance { get { return instance; } }//private set; }
+
+    private static TitleAnyKeyInput instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else
+        {
+            Destroy(gameObject);
+            Debug.LogError("TitleAnyKeyInputのインスタンスが失敗しました。");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetKey//检测键盘输入
+        //Input.GetMouseButton//检测鼠标输入
+        //Input.GetAxis//检测在Input Manager中定义的虚拟轴的值
+        //Input.GetButton//检测在Input Manager中定义的虚拟按钮是否被按下
+
+        if(Input.anyKeyDown || MouseAnyKeyDown())
+        {
+            OnAnyKeyDown?.Invoke();
+        }
+
+    }
+
+    bool MouseAnyKeyDown()
+    {
+        if (Input.GetMouseButtonDown(0) ||
+            Input.GetMouseButtonDown(1) ||
+            Input.GetMouseButtonDown(2))
+            return true;
+
+        return false;
+    }
+}
