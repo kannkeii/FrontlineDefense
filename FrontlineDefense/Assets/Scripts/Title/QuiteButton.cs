e7cb41d475e85027ebdb54b952c5c33985811c6b
kannkeii <kannkeii2015@gmail.com>
2023-08-31 23:23:45 +0900

    [add]TitleScene_UI_QuietButtonの機能作成

---
+++ FrontlineDefense/Assets/Scripts/Title/QuiteButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuiteButton : MonoBehaviour
+{
+    private bool lastMuteValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text text = GetComponentInChildren<Text>();
+
+        text.text = "♪";
+
+
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            AudioController.Instance.Change();
+            if (AudioController.Instance.GetStatus() != lastMuteValue)
+            {
+                if(AudioController.Instance.GetStatus() == true)
+                    text.text = '\u0336'+"♪" + '\u0336';
+                else
+                    text.text = "♪";
+
+                lastMuteValue = AudioController.Instance.GetStatus();
+            }
+        }
+        );
+
+        lastMuteValue = AudioController.Instance.GetStatus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
